78e9046ae8d5d17a200dd62bf1613708f2f2cbf7
SBIA-Younes <brahim-sb@hotmail.fr>
2023-11-24 18:37:11 +0100

    CSS module

--- React/Formulairs/forms/src/Form.js
+++ React/Formulairs/forms/src/Form.js
@@ -1,18 +1,19 @@
 import { Component } from "react";
+import './myCss.css'
+import MyHead from "./myHeaderOne";
+import styles from './myCss.module.css';
 
 
-const titreRouge = {
-  frontSize: '50px',
-  color:'red'
-}
 
 class Form extends Component {
 
-
   render() {
+    const myClass = this.props.head ? 'blue' : 'red';
+
     return (
     <div>
-      <h1 style={titreRouge}>Commentaire</h1>
+      <MyHead/>
+      <p className={styles.green}>je suis rouge ou bleu</p>
       <button>Valider</button>
     </div>
     )
---
+++ React/Formulairs/forms/src/myHeaderOne.js
@@ -0,0 +1,11 @@
+
+
+const MyHead = ()=> {
+  return ( 
+  <div>
+    <h1 >Commentaire</h1>
+    <p className="blue">je suis rouge ou bleu</p>
+  </div>
+)}
+
+export default MyHead;
