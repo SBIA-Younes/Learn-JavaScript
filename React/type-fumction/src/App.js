90f80c32f0508fb8ffa56f72b54821c4128affd6
SBIA-Younes <brahim-sb@hotmail.fr>
2023-12-16 19:40:39 +0100

    Comparation between Function abd Class

---
+++ React/type-fumction/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import ClassState from './components/ClassState';
+import FunctionState from './components/FunctionState';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='text-center'>userState Hooks</h1>
+      <ClassState/>
+      <FunctionState/>
+    
+    </div>
+  );
+}
+
+export default App;
