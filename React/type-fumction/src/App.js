f190a37d198e854da7cee8190d75865f9daaf083
SBIA-Younes <brahim-sb@hotmail.fr>
2023-12-18 21:21:00 +0100

    How to use useReduce with object

--- React/type-fumction/src/App.js
+++ React/type-fumction/src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
   render (){
     return (
     <div className='container'>
-      <h1>Test</h1>
+
       <Count/>
     </div>
   );
--- React/type-fumction/src/components/Count.js
+++ React/type-fumction/src/components/Count.js
@@ -1,13 +1,21 @@
 import React, { useReducer } from 'react'
 
-const initialState = 0;
+const initialState = {
+  countOne:0,
+  countTwo:0
+};
 
 const reducer = (state, action) => {
-  switch(action) {
-    case "INCREMENT":
-      return state + 1;
-    case "DECREMENT":
-        return state - 1;
+  console.log(state);
+  switch(action.type) {
+    case "INCREMENT_One":
+      return {...state, countOne: state.countOne + action.value};
+    case "DECREMENT_One":
+        return {...state, countOne: state.countOne - action.value};
+    case "INCREMENT_Two":
+      return {...state, countTwo: state.countTwo + action.value};
+    case "DECREMENT_Two":
+        return {...state, countTwo: state.countTwo - action.value};
     case "REINITIALISER":
         return initialState;
     default:
@@ -19,11 +27,20 @@ function Count() {
 
   const [count, dispath] = useReducer(reducer, initialState)
   return (
-    <div>
-      <h2>Count: {count}</h2>
-      <button className='btn btn-success m-3' onClick={()=>{dispath('INCREMENT')}}>+</button>
-      <button className='btn btn-danger m-3' onClick={()=>{dispath('DECREMENT')}}>-</button>
-      <button className='btn btn-warning m-3' onClick={()=>{dispath('REINITIALISER')}}>0</button>
+    <div className='row'>
+      <div className='col-6 text-center'>
+        <h2>Count: {count.countOne}</h2>
+        <button className='btn btn-success m-3' onClick={()=>{dispath({type:'INCREMENT_One',value:1})}}>+</button>
+        <button className='btn btn-danger m-3' onClick={()=>{dispath({type:'DECREMENT_One',value:1})}}>-</button>
+      </div>
+      <div className='col-6 text-center'>
+        <h2>Count: {count.countTwo}</h2>
+        <button className='btn btn-success m-3' onClick={()=>{dispath({type:'INCREMENT_Two',value:10})}}>+</button>
+        <button className='btn btn-danger m-3' onClick={()=>{dispath({type:'DECREMENT_Two',value:10})}}>-</button>
+      </div>
+        <div className='col text-center'>
+          <button className='btn btn-warning m-3' onClick={()=>{dispath({type:'REINITIALISER'})}}>0</button>
+        </div>
     </div>
   )
 }
