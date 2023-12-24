d92c1b85513387984879341faac3e7e24b654d2b
SBIA-Younes <brahim-sb@hotmail.fr>
2023-12-24 07:55:09 +0100

    Add useDebugValue to log data in custom hook

--- React/type-fumction/src/hooks/useFetch.js
+++ React/type-fumction/src/hooks/useFetch.js
@@ -1,9 +1,17 @@
-import { useEffect, useState } from "react"
+import { useDebugValue, useEffect, useState } from "react"
 
 const useFetch = (fetchUrl)=> {
 
   const [data, setData] = useState([])
   const [isLoading, setIsLoading] = useState(true)
+
+   useDebugValue(data, val => {
+
+    alert('useDebugValue')
+    return JSON.stringify(val)
+  })
+  // Je suis en mode DEV
+  // React-dev-Tool ouvert pour inspecter le custom hook
 
   useEffect(() => {
     fetch(fetchUrl)
