85fea451978555d8c5c495c6b1597928bde5af97
SBIA-Younes <brahim-sb@hotmail.fr>
2023-12-28 08:20:48 +0100

    set app search with fetch

---
+++ React/type-fumction/src/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import MyContacts from './components/MyContacts';
+
+const App = () => {
+    return (
+        <div className='App'>
+            <h1>Mes Contacts</h1>
+           <MyContacts />
+        </div>
+    )
+}
+export default App;
